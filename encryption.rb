cd26c330e550df790fa82a7c0252dc1946f00d61
leont <leon.traykov@gmail.com>
2022-04-23 22:27:45 +0700

    Merge with SHA2 branch

--- encryption.rb
+++ encryption.rb
@@ -6,6 +6,7 @@ secret = $stdin.gets.chomp
 puts "Каким способом зашифровать:"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 way_to_keep = $stdin.gets.chomp
 
 puts case way_to_keep 
@@ -15,6 +16,9 @@ when "1"
 when "2"
   "Вот что получилось:"
   Digest::SHA1.hexdigest secret
+when "3"
+  "Вот что получилось:"
+  Digest::SHA2.hexdigest secret
 else 
   "Нет такого способа"
 end
